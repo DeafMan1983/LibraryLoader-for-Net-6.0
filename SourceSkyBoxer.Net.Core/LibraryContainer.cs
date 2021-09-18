41e63a6c87e9b3a094ca97060dd03c1c5645a638
DeafMan1983 <57066679+DeafMan1983@users.noreply.github.com>
2021-09-18 10:59:11 +0200

    Add files via upload

---
+++ SourceSkyBoxer.Net.Core/LibraryContainer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SourceSkyBoxer.Net.Core
+{
+    public enum Platform
+    {
+        Unknown,
+        Windows,
+        Linux,
+        Android,
+        MacOS,
+        IOS
+    }
+
+    public abstract class LibraryContainer
+    {
+        public static Platform Platform { get; set; } = RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+            ? RuntimeInformation.IsOSPlatform(OSPlatform.Create("ANDROID"))
+                ? Platform.Android
+                : Platform.Linux
+            : RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? Platform.Windows
+            : RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+                ? RuntimeInformation.IsOSPlatform(OSPlatform.Create("IOS"))
+                    ? Platform.IOS 
+                    : Platform.MacOS
+            : Platform.Unknown;
+
+        public string GetLibraryPath => Platform switch
+        {
+            Platform.Unknown => ThrowInvalidPlatform(),
+            Platform.Windows => Windows,
+            Platform.Linux => Linux,
+            Platform.Android => Android,
+            Platform.MacOS => MacOS,
+            Platform.IOS => IOS,
+            _ => ThrowInvalidPlatform()
+        };
+
+        public abstract string Windows { get; }
+
+        public abstract string Linux { get; }
+
+        public abstract string MacOS { get; }
+
+        public virtual string Android => Linux;
+
+        public virtual string IOS => MacOS;
+
+        private static string ThrowInvalidPlatform()
+        {
+            throw new PlatformNotSupportedException("Invalid/unsupported operating system.");
+        }
+    }
+}
---
+++ SourceSkyBoxer.Net.Core/LibraryLoader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SourceSkyBoxer.Net.Core
+{
+    public abstract class LibraryLoader 
+    {
+        private string _preifx;
+        private nint libraryhandle, _librarysymbol;
+
+        /*
+         *  Load library and prefix example "SDL_", "gtk_" etc
+         */
+        public LibraryLoader(string librarypath, string preifx = null)
+        {
+            _preifx = preifx;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                libraryhandle = Kernel32.LoadLibrary(librarypath);
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                libraryhandle = Libdl.dlopen(librarypath, Libdl.RtldNow);
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                libraryhandle = Libdl.dlopen(librarypath, Libdl.RtldNow);
+            else
+                throw new PlatformNotSupportedException("Error: Nothing uses for current platform.");
+        }
+
+
+        /*
+         *  LoadMethod example "SDL_Init" If Load library passes with preifx "SDL_" then "Init"
+         */
+        public T LoadMethod<T>(string methodname)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (_preifx == null)
+                    _librarysymbol = Kernel32.GetProcAddress(libraryhandle, methodname);
+                else
+                    _librarysymbol = Kernel32.GetProcAddress(libraryhandle, _preifx+methodname);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                if (_preifx == null)
+                    _librarysymbol = Libdl.dlsym(libraryhandle, methodname);
+                else
+                    _librarysymbol = Libdl.dlsym(libraryhandle, _preifx + methodname);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                if (_preifx == null)
+                    _librarysymbol = Libdl.dlsym(libraryhandle, methodname);
+                else
+                    _librarysymbol = Libdl.dlsym(libraryhandle, _preifx + methodname);
+            }
+            else
+                throw new NotImplementedException("Error: Method or Implement doesn't find in library.");
+
+            return Marshal.GetDelegateForFunctionPointer<T>(_librarysymbol);
+        }
+    }
+}
